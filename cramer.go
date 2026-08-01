9aab0fa8a3b0918fb4d688f865fb97676e2a8c32
archimagefenix <archimagefenix@proton.me>
2026-07-31 21:05:48 -0600

    Creacion de funcion para llenar y para imprimir matriz n

--- cramer.go
+++ cramer.go
@@ -12,9 +12,13 @@ func leern() {
 
 	var n int
 
-	fmt.Println("Ingrese el valor de n: ")
+	fmt.Println("\nIngrese el valor de n: ")
 	fmt.Scan(&n)
-	prepararMatriz(n)
+
+	matriz := prepararMatriz(n)
+
+	leerCoeficientes(matriz, n)
+	imprimirMatriz(matriz, n)
 
 }
 
@@ -25,13 +29,41 @@ func prepararMatriz(n int) [][]float64 {
 	// Crear las columnas de cada fila
 	for i := 0; i < n; i++ { //[][]  crear un slice cuyos elementos también serán slices.
 
-		matriz[i] = make([]float64, n)
+		matriz[i] = make([]float64, n+1)
 
 	}
 
+	return matriz
+
+}
+
+func leerCoeficientes(matriz [][]float64, n int) {
+
 	for i := 0; i < n; i++ {
 
-		for j := 0; j < n; j++ {
+		fmt.Printf("\nEcuación %d\n", i+1)
+
+		for j := 0; j < n+1; j++ {
+
+			if j == n {
+				fmt.Print("Resultado: ")
+			} else {
+				fmt.Printf("Coeficiente X%d: ", j+1)
+			}
+
+			fmt.Scan(&matriz[i][j])
+		}
+	}
+
+}
+
+func imprimirMatriz(matriz [][]float64, n int) {
+
+	fmt.Println("\nMatriz aumentada:\n")
+
+	for i := 0; i < n; i++ {
+
+		for j := 0; j < n+1; j++ {
 
 			fmt.Printf("%8.2f", matriz[i][j])
 
@@ -40,7 +72,4 @@ func prepararMatriz(n int) [][]float64 {
 		fmt.Println()
 
 	}
-
-	return matriz
-
 }
--- venta.go
+++ venta.go
@@ -6,21 +6,25 @@ func Venta() {
 
 	var precio, cantidad float64 //DECLARACION DE VARIABLES FLOAT64
 
-	fmt.Println("\tPrograma de Calculo de Precio de Compra ")
-	fmt.Println("Ingrese el Precio del Articulo (C$) ")
+	fmt.Println("\nPrograma de Calculo de Precio de Compra ")
+	fmt.Println("\nIngrese el Precio del Articulo (C$) ")
 	fmt.Scan(&precio)
-	fmt.Println("Ingrese el Cantidad de Articulos: (C$)")
+	fmt.Println("\nIngrese el Cantidad de Articulos: (C$)")
 	fmt.Scan(&cantidad)
 	calcularPreciocompra(precio, cantidad) //FUNCION QUE ENVIA
-	//VALORES A
+	//VALORES A LA FUNCION calcularPreciocompra
 
 }
 
 func calcularPreciocompra(precio float64, cantidad float64) {
+	//ESTA FUNCION OBTIENE DOS PARAMETROS TIPO FLOAT QUE RCIBE DE MAIN
+	// EL OBJETIVO ES QUE ESTA CALCULE EL PRECIO QUE ELCLIENTE DEBE PAGAR
+	//EN BASE A LA CANTIDAD DE ARTICULOS DEJANDO POR TANTO A MAIN COMO
+	//LECTOR DE DATOS SOLAMENTE
+	var precioFinal float64 //DECLARACION DE VARIABLE
 
-	var precioFinal float64
+	precioFinal = (precio * cantidad)
 
-	precioFinal = precio * cantidad
 	fmt.Printf("\nEl Precio a Pagar es de: C$ %.2f", precioFinal)
 
 }
